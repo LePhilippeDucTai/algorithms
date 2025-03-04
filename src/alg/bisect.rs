eba657f9a19b16100b81fd4817ae25df0e44625a
Georges SARR <georgesmbissanes@gmail.com>
2025-03-04 19:26:55 +0100

    Made bisect_f generic for floats.

--- src/alg/bisect.rs
+++ src/alg/bisect.rs
@@ -1,10 +1,16 @@
+use rand_distr::num_traits::Float;
 use time_it_macro::time_it;
+// use
 
 #[time_it]
-pub fn bisect_f<T: Fn(f64) -> f64>(a: f64, b: f64, f: &T) -> (f64, f64) {
-    let m = (a + b) * 0.5;
+pub fn bisect_f<T, F>(a: T, b: T, f: F) -> (T, T)
+where
+    F: Fn(T) -> T,
+    T: Float,
+{
+    let m = (a + b) / T::from(2).unwrap();
     let (left, mid) = (f(a), f(m));
-    if left * mid < 0.0 {
+    if left * mid < T::zero() {
         return (a, m);
     }
     (m, b)
@@ -21,4 +27,15 @@ pub fn bisect<T: Fn(f64) -> f64>(f: T, a: f64, b: f64, eps: f64) -> (f64, f64) {
     res.take_while(|&(x, y)| (y - x).abs() > eps)
         .last()
         .unwrap_or((a, b))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_bisect_f() {
+        let f = |x| x;
+        assert_eq!(bisect_f(-1., 1., f), (0., 1.));
+        assert_eq!(bisect_f(-2., 1., f), (-0.5, 1.));
+    }
 }
